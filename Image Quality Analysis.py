514f49b79fae6d42eb8d0a09e17123cd5b47292e
Siddharth Kumar Sah <siddharth123sk@gmail.com>
2023-07-28 18:19:23 +0800

    Update Image Quality Analysis.py

--- Image Quality Analysis.py
+++ Image Quality Analysis.py
@@ -1,26 +1,61 @@
+"""
+Filename: image_blurriness_detection.py
+
+This script reads an image and determines its blurriness score using the Laplacian 
+method. A higher score means the image is in focus, while a low score implies the 
+image is blurry. The script will print 'Blurry' or 'Not blurry' based on the score.
+
+Author: Siddharth Kumar (www.siddharthsah.com)
+Last Updated: July 28, 2023
+"""
+
 import cv2
 from PIL import Image
 import numpy as np
 
-#image = cv2.imread('./data/front.png')  # read the image
 
+def calculate_blurriness_score(image_path):
+    """
+    Calculates the blurriness score of the image located at the given path.
 
-image_path = './data/front.png'
+    Args:
+        image_path: The path to the image file.
 
+    Returns:
+        The blurriness score as a float.
+    """
 
-image = Image.open(image_path).convert("RGB")
-image = np.array(image)
+    # Open the image file and convert it to RGB
+    image = Image.open(image_path).convert("RGB")
+    image = np.array(image)
 
-def blurriness(image):
-    # converting the image into the grayscale
+    # Convert the image into grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # calculate laplacian of the image
-    return cv2.Laplacian(image, cv2.CV_64F).var()
 
-print(blurriness(image))
+    # Calculate laplacian of the image and return the variance
+    return cv2.Laplacian(gray, cv2.CV_64F).var()
 
-if blurriness(image) > 35:
-    print("Not blurry")
-else:
-    print("Blurry")
 
+def main():
+    """
+    The main function of the script. It determines and prints the blurriness status 
+    of the image at the given path.
+    """
+
+    # The path to the image file
+    image_path = './data/front.png'
+
+    # Calculate the blurriness score of the image
+    blurriness_score = calculate_blurriness_score(image_path)
+
+    print(f"Blurriness Score: {blurriness_score}")
+
+    # Determine whether the image is blurry or not
+    if blurriness_score > 35:
+        print("Status: Not blurry")
+    else:
+        print("Status: Blurry")
+
+
+if __name__ == "__main__":
+    main()
