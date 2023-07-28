4dd3f0aa4053759151f7ad81a153adb52a362460
Siddharth Kumar Sah <siddharth123sk@gmail.com>
2023-07-28 18:14:05 +0800

    Update Rotate Image by a certain angle.py

--- Rotate Image by a certain angle.py
+++ Rotate Image by a certain angle.py
@@ -1,30 +1,81 @@
-import numpy as np
+"""
+filename: image_rotation.py
+
+This is an open-source Python script for image processing using OpenCV. 
+The script reads an image, determines its center, and then rotates the image around this center point.
+
+Author: Siddharth Kumar (www.siddharthsah.com)
+Last Updated: July 28, 2023
+"""
+
 import argparse
-import imutils
 import cv2
+import imutils
+import numpy as np
 import matplotlib.pyplot as plt
 
+def load_image(image_path):
+    """
+    Loads an image from the specified file path.
 
-# Load and display original image
-image = cv2.imread('./data/output/deskewed/File 1.jpg')
+    Args:
+        image_path (str): The file path of the image to load.
 
+    Returns:
+        ndarray: The loaded image.
+    """
+    try:
+        return cv2.imread(image_path)
+    except IOError as e:
+        print(f"IOError: {e}")
+        print(f"Could not read file at path: {image_path}")
+        return None
 
-# Determine the center of the image
-# OpenCV allows you to specify any arbitrary point to rotate around,
-# here we'll use the image center
-(h, w) = image.shape[:2]
-center = (w//2, h//2)
+def rotate_image(image, angle):
+    """
+    Rotates an image by the specified angle around its center point.
 
-# Build the rotation matrix around the center, 45 degrees clockwise,
-# without changing the scale
-# cv2.warpAffine applies the transformation
-angleToRotate = -20
-M = cv2.getRotationMatrix2D(center, angleToRotate, 1.0)
-rotated = cv2.warpAffine(image, M, (w, h))
-# cv2.imshow("Rotated by 45 Degrees", rotated)
-# cv2.waitKey(0)
+    Args:
+        image (ndarray): The image to rotate.
+        angle (float): The angle by which to rotate the image.
 
-plt.imshow(rotated)
-plt.show()
+    Returns:
+        ndarray: The rotated image.
+    """
+    if image is not None:
+        (h, w) = image.shape[:2]
+        center = (w // 2, h // 2)
+        M = cv2.getRotationMatrix2D(center, angle, 1.0)
+        rotated = cv2.warpAffine(image, M, (w, h))
+        return rotated
+    else:
+        print("Image is None, cannot perform rotation.")
+        return None
 
+def display_image(image):
+    """
+    Displays an image using matplotlib.
 
+    Args:
+        image (ndarray): The image to display.
+    """
+    if image is not None:
+        plt.imshow(image)
+        plt.show()
+    else:
+        print("Image is None, cannot display image.")
+
+def main():
+    """
+    The main function to execute the image processing.
+    """
+    image_path = './data/output/deskewed/File 1.jpg'
+    image = load_image(image_path)
+
+    angle_to_rotate = -20
+    rotated_image = rotate_image(image, angle_to_rotate)
+
+    display_image(rotated_image)
+
+if __name__ == "__main__":
+    main()
