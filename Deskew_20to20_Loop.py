fe4ea30095bde49390c27181a44eff6cc47e59f6
Siddharth Kumar Sah <siddharth123sk@gmail.com>
2023-07-28 18:01:37 +0800

    Update Deskew_20to20_Loop.py

--- Deskew_20to20_Loop.py
+++ Deskew_20to20_Loop.py
@@ -1,260 +1,134 @@
+# id_card_data_extraction.py
+
+import os
+import re
+from typing import Tuple, List
+
+import cv2
 import numpy as np
+import pandas as pd
+import pytesseract
+from PIL import Image, ImageOps
+from deskew import determine_skew
 from skimage import io
 from skimage.color import rgb2gray
 from skimage.transform import rotate
-import cv2
-# importing the necessary packages
-import os
-import re
-import cv2
-import pytesseract
-from PIL import Image, ImageOps
-import matplotlib.pyplot as plt
-import pandas as pd
 
-from deskew import determine_skew
+# Global constants
+DIRECTORY = './Deskewed Cropped/'
+OUTPUT_CSV = "./Deskewed Cropped/result_Deskewed_Cropped_Rotated_100to100.csv"
+IMG_DIM = (1024, 768)
+PYTESSERACT_CONFIG = '-l eng --oem 3 --psm 6'
 
 
-# this functions take the front image of the ID card and returns a string of all the text information in the image
-def textExtractorfront_jpg(image_location):
-    # you can use both jpg and png file formats here
-    #img = cv2.imread(image_location)
+def preprocess_image(image_location: str) -> np.array:
+    """
+    Preprocess the image to be ready for tesseract
 
+    :param image_location: Path to the image file
+    :return: Processed image as a numpy array
+    """
     img = Image.open(image_location).convert("RGB")
-
     image = ImageOps.exif_transpose(img)
-
     img = np.array(image)
-
-    # plt.imshow(img)
-    # plt.show()
-
-    # img = Image.open('./Original Images/File91.jpg')
-
-    img = cv2.resize(img, (1024, 768))
-
-    # def rotate(image, center=None, scale=1.0):
-    #     angle = 360 - int(re.search('(?<=Rotate: )\d+', pytesseract.image_to_osd(image)).group(0))
-    #     (h, w) = image.shape[:2]
-    #
-    #     if center is None:
-    #         center = (w / 2, h / 2)
-    #
-    #     # Perform the rotation
-    #     M = cv2.getRotationMatrix2D(center, angle, scale)
-    #     rotated = cv2.warpAffine(image, M, (w, h))
-    #
-    #     return rotated
-    #
-    # img = rotate(img)
-
-    # rot_data = pytesseract.image_to_osd(img, );
-    # print("[OSD] " + rot_data)
-    # rot = re.search('(?<=Rotate: )\d+', rot_data).group(0)
-    #
-    # angle = float(rot)
-    # if angle > 0:
-    #     angle = 360 - angle
-    # print("[ANGLE] " + str(angle))
-    #
-    # (h, w) = img.shape[:2]
-    # center = (w // 2, h // 2)
-    # M = cv2.getRotationMatrix2D(center, angle, 1.0)
-    # img = cv2.warpAffine(img, M, (w, h),
-    #                          flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
-
-    #print(pytesseract.image_to_osd(img, config='--psm 0'))
+    img = cv2.resize(img, IMG_DIM)
+    return img
 
 
+def text_extractor(image_location: str) -> str:
+    """
+    Extract the text from an image
 
-    # plt.imshow(img)
-    # plt.show()
-
-    #img = cv2.imread('front.jpg')
-
-
-    #gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    #config = ('-l eng --oem 1 --psm 3')
-
-    #string = (pytesseract.image_to_string(img, config= config))
-    # string = (pytesseract.image_to_string(img))
-
-    config = '-l eng --oem 3 --psm 6'
-
-    string = (pytesseract.image_to_string(img, config=config))
-
-
-    #print(string)
-
+    :param image_location: Path to the image file
+    :return: Extracted text as a string
+    """
+    img = preprocess_image(image_location)
+    string = pytesseract.image_to_string(img, config=PYTESSERACT_CONFIG)
     return string
 
 
-# this functions uses a regex to find the FIN  in the string returned from the textExtractorfront_jpg function and returns the FIN
-def FIN_ExtractorFromString(image_location):
+def fin_extractor(image_location: str) -> str:
+    """
+    Extract the FIN from the text extracted from the image
 
-    FIN = "None"
-    # getting the string of text from the image
-    string = textExtractorfront_jpg(image_location)
-    #print(string)
-
-    # converting this string into an array by splitting it with the space
+    :param image_location: Path to the image file
+    :return: Extracted FIN as a string
+    """
+    string = text_extractor(image_location)
     store_array = string.split()
 
-    # we do an element wise search to find which element in the array matches with regex of a FIN, this regex will work for both local and foreigner FIN
     for element in store_array:
+        if re.match("^[STFG]\d{7}[A-Z]$", element):
+            return element
 
-        m = re.match("^[STFG]\d{7}[A-Z]$", element)
-
-        # See if success.
-        if m:
-            # print(element)
-            FIN = element
-
-        else:
-            pass
-            # print("No FIN found!")
-
-    return FIN
+    return "None"
 
 
-def rotation(rotateImage, angle):
-    # Taking image height and width
-    imgHeight, imgWidth = rotateImage.shape[0], rotateImage.shape[1]
+def rotate_image(image: np.array, angle: float) -> np.array:
+    """
+    Rotate the image
 
-    # Computing the centre x,y coordinates
-    # of an image
-    centreY, centreX = imgHeight // 2, imgWidth // 2
+    :param image: Image as a numpy array
+    :param angle: Angle to rotate the image
+    :return: Rotated image as a numpy array
+    """
+    img_height, img_width = image.shape[:2]
+    centre_y, centre_x = img_height // 2, img_width // 2
+    rotation_matrix = cv2.getRotationMatrix2D((centre_y, centre_x), angle, 1.0)
+    cos_val = np.abs(rotation_matrix[0][0])
+    sin_val = np.abs(rotation_matrix[0][1])
 
-    # Computing 2D rotation Matrix to rotate an image
-    rotationMatrix = cv2.getRotationMatrix2D((centreY, centreX), angle, 1.0)
+    new_image_height = int((img_height * sin_val) + (img_width * cos_val))
+    new_image_width = int((img_height * cos_val) + (img_width * sin_val))
 
-    # Now will take out sin and cos values from rotationMatrix
-    # Also used numpy absolute function to make positive value
-    cosofRotationMatrix = np.abs(rotationMatrix[0][0])
-    sinofRotationMatrix = np.abs(rotationMatrix[0][1])
+    rotation_matrix[0][2] += (new_image_width / 2) - centre_x
+    rotation_matrix[1][2] += (new_image_height / 2) - centre_y
 
-    # Now will compute new height & width of
-    # an image so that we can use it in
-    # warpAffine function to prevent cropping of image sides
-    newImageHeight = int((imgHeight * sinofRotationMatrix) +
-                         (imgWidth * cosofRotationMatrix))
-    newImageWidth = int((imgHeight * cosofRotationMatrix) +
-                        (imgWidth * sinofRotationMatrix))
-
-    # After computing the new height & width of an image
-    # we also need to update the values of rotation matrix
-    rotationMatrix[0][2] += (newImageWidth / 2) - centreX
-    rotationMatrix[1][2] += (newImageHeight / 2) - centreY
-
-    # Now, we will perform actual image rotation
-    rotatingimage = cv2.warpAffine(
-        rotateImage, rotationMatrix, (newImageWidth, newImageHeight))
-
-    return rotatingimage
+    rotating_image = cv2.warpAffine(image, rotation_matrix, (new_image_width, new_image_height))
+    return rotating_image
 
 
-# filename = 'File 1.jpg'
-#
-# image = io.imread('./data/output/' + filename)
-# grayscale = rgb2gray(image)
-# angle = determine_skew(grayscale)
-# rotated = rotate(image, angle, resize=True) * 255
-# height, width = rotated.shape[:2]
-# # print(height)
-# # print(width)
-# if width > height:
-#     io.imsave('./data/output/deskewed/temp/' + filename, rotated.astype(np.uint8))
-#     image = rotated
-# else:
-#     image = cv2.rotate(rotated, cv2.cv2.ROTATE_90_COUNTERCLOCKWISE)
-#     io.imsave('./data/output/deskewed/temp/' + filename, image.astype(np.uint8))
-# #print('Done!')
+def process_images() -> Tuple[np.array, np.array, np.array]:
+    """
+    Process all images in a directory
 
-directory = './Deskewed Cropped/'
+    :return: Filenames, FINs, and correctness as numpy arrays
+    """
+    filenames = []
+    fins = []
+    is_fin_correct = []
 
-#array = []
-filenameArray = []
-FINArray = []
-IsFINCorrect = []
+    for filename in os.listdir(DIRECTORY):
+        if filename.endswith(".jpg"):
+            image_path = os.path.join(DIRECTORY, filename)
+            image = cv2.imread(image_path)
+            for i in range(-100, 100):
+                rotated = rotate_image(image, i)
+                temp_path = f'./Deskewed Cropped/temp/rotated_{i}.jpg'
+                cv2.imwrite(temp_path, rotated)
 
-for filename in os.listdir(directory):
+                fin = fin_extractor(temp_path)
+                if fin == "":
+                    break
 
-    if filename.endswith(".jpg"):
-        #do smth
+            is_fin_correct.append('1' if fin == '' else '0')
+            filenames.append(filename)
+            fins.append(fin)
 
-        #print(filename)
-
-        #image = cv2.imread(directory + filename)
-        # import numpy as np
-        # import argparse
-        # import imutils
-        # import cv2
-        # import matplotlib.pyplot as plt
-
-        # Load and display original image
-        image = cv2.imread(directory + filename)
-
-        # Determine the center of the image
-        # OpenCV allows you to specify any arbitrary point to rotate around,
-        # here we'll use the image center
-
-        for i in range(-100, 100):
-
-            rotated = rotation(image, i)
-
-            # (h, w) = image.shape[:2]
-            # center = (w // 2, h // 2)
-            #
-            # # Build the rotation matrix around the center, 45 degrees clockwise,
-            # # without changing the scale
-            # # cv2.warpAffine applies the transformation
-            # #print(i)
-            # angleToRotate = i
-            # M = cv2.getRotationMatrix2D(center, angleToRotate, 1.0)
-            # rotated = cv2.warpAffine(image, M, (w, h))
-            # cv2.imshow("Rotated by 45 Degrees", rotated)
-            # cv2.waitKey(0)
-
-            # plt.imshow(rotated)
-            # plt.show()
-
-            #image = rotated
-            image_location = './Deskewed Cropped/temp/'+ 'rotated_' + str(i) + '.jpg'
-            print(image_location)
-            cv2.imwrite('./Deskewed Cropped/temp/'+ 'rotated_' + str(i) + '.jpg', rotated)
-
-            FIN = FIN_ExtractorFromString(image_location)
-            print(FIN)
-
-            if (FIN == ""):
-                break
-
-        if FIN == '':
-            IsFINCorrect.append('1')
-        else:
-            IsFINCorrect.append('0')
-
-        filenameArray.append(filename)
-        FINArray.append(FIN)
+    return np.array(filenames), np.array(fins), np.array(is_fin_correct)
 
 
+def main():
+    try:
+        filenames, fins, is_fin_correct = process_images()
+        df = pd.DataFrame({"Filename": filenames, "FIN": fins, "Correct FIN?": is_fin_correct})
+        df.to_csv(OUTPUT_CSV, index=False)
+        print('Done!')
+
+    except Exception as e:
+        print(f"Error encountered: {str(e)}")
+        raise e
 
 
-
-
-
-
-        # if __name__ == '__main__':
-        #     print(FIN_ExtractorFromString())
-
-
-
-#array = np.array(array)
-filenameArray = np.array(filenameArray)
-FINArray = np.array(FINArray)
-IsFINCorrect = np.array(IsFINCorrect)
-
-df = pd.DataFrame({"Filename" : filenameArray, "FIN": FINArray, "Correct FIN?": IsFINCorrect})
-df.to_csv("./Deskewed Cropped/result_Deskewed_Cropped_Rotated_100to100.csv", index=False)
-print('Done!')
+if __name__ == '__main__':
+    main()
