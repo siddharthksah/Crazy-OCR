530125ad84c984a7a3f310956009b296f56e8e70
Siddharth Kumar Sah <siddharth123sk@gmail.com>
2023-07-28 17:56:10 +0800

    Update Collage Maker.py

--- Collage Maker.py
+++ Collage Maker.py
@@ -1,45 +1,69 @@
+"""
+Module to create a collage from a collection of images.
+"""
+
 from PIL import Image
 import os
 
-#write the number of columns and rows you want in your collage
-cols = 10
-rows = 10
 
-#define the ouput image size
-width = 10240
-height = 7680
+def make_collage(directory, output_path, cols=10, rows=10, width=10240, height=7680, debug=False):
+    """
+    Function to create a collage from images in a specified directory.
 
+    :param directory: str, Path to directory containing images.
+    :param output_path: str, Path to save the generated collage.
+    :param cols: int, Number of columns in the collage. Default is 10.
+    :param rows: int, Number of rows in the collage. Default is 10.
+    :param width: int, Width of the collage. Default is 10240.
+    :param height: int, Height of the collage. Default is 7680.
+    :param debug: bool, Flag to print debug information. Default is False.
+    """
+    thumbnail_width = width // cols
+    thumbnail_height = height // rows
+    size = thumbnail_width, thumbnail_height
+    new_im = Image.new('RGB', (width, height))
+    ims = []
 
-thumbnail_width = width//cols
-thumbnail_height = height//rows
-size = thumbnail_width, thumbnail_height
-new_im = Image.new('RGB', (width, height))
-ims = []
+    # Get list of files in directory
+    files = os.listdir(directory)
 
-#this is the directory from where all the images will be taken, please make sure your images are either .jpg or .png for it make
-#basically all the images inside this folder will be used to create the collage
-directory = './Deskewed Cropped/temp/'
+    # Check if enough images to fill collage
+    if len(files) < cols * rows:
+        print("Error: Not enough images in directory to fill collage.")
+        return
 
-for filename in os.listdir(directory):
-    #if filename.endswith(".jpg" or ".png"):
-    if filename.endswith(".jpg"):
-        im = Image.open(directory + filename)
-        im.thumbnail(size)
-        ims.append(im)
+    for filename in files:
+        if filename.endswith(".jpg"):
+            try:
+                im = Image.open(os.path.join(directory, filename))
+                im.thumbnail(size)
+                ims.append(im)
+            except IOError:
+                print(f"Error: Unable to open image file {filename}. Skipping...")
 
-i = 0
-x = 0
-y = 0
-for col in range(cols):
-    for row in range(rows):
-        print(i, x, y)
-        new_im.paste(ims[i], (x, y))
-        i += 1
-        y += thumbnail_height
-    x += thumbnail_width
+    i = 0
+    x = 0
     y = 0
 
-# the collage in this case will be saved in the root directory with the name 'collage.jpg'
-new_im.save("./Deskewed Cropped/temp/collage.png")
+    for col in range(cols):
+        for row in range(rows):
+            if debug:
+                print(i, x, y)
+            new_im.paste(ims[i], (x, y))
+            i += 1
+            y += thumbnail_height
+        x += thumbnail_width
+        y = 0
+
+    try:
+        new_im.save(output_path)
+        print(f"Collage saved successfully at {output_path}")
+    except IOError:
+        print("Error: Unable to save collage.")
 
 
+if __name__ == "__main__":
+    input_dir = "./Deskewed Cropped/temp/"
+    output_path = "./Deskewed Cropped/temp/collage.png"
+
+    make_collage(input_dir, output_path, debug=True)
